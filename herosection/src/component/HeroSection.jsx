47828e207385293c0d423f98c96ddf4e8f400407
mesatyamsharma123 <96259558+mesatyamsharma123@users.noreply.github.com>
2025-04-16 00:30:36 +0530

    l

---
+++ herosection/src/component/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HeroSection() {
+  return (
+    <div className=''>
+      <h2 className='text-3xl'>heleo</h2>
+    </div>
+  )
+}
+
+export default HeroSection
